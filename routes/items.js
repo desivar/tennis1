07427b0279ede318061f12d374723c8f118624ab
desivar <jillian_desire@yahoo.es>
2025-04-12 03:54:00 -0600

    update

--- routes/items.js
+++ routes/items.js
@@ -1,78 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const Item = require('../models/Item');
-
-// GET all items
-router.get('/', async (req, res) => {
-  try {
-    const items = await Item.find();
-    res.json(items);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// GET one item
-router.get('/:id', getItem, (req, res) => {
-  res.json(res.item);
-});
-
 // POST a new item
 router.post('/', async (req, res) => {
-  const item = new Item({
-    name: req.body.name,
-    description: req.body.description
-  });
-
-  try {
-    const newItem = await item.save();
-    res.status(201).json(newItem);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// PUT (update) an item
-router.put('/:id', getItem, async (req, res) => {
-  if (req.body.name != null) {
-    res.item.name = req.body.name;
-  }
-  if (req.body.description != null) {
-    res.item.description = req.body.description;
-  }
-
-  try {
-    const updatedItem = await res.item.save();
-    res.json(updatedItem);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// DELETE an item
-router.delete('/:id', getItem, async (req, res) => {
-  try {
-    await res.item.remove();
-    res.json({ message: 'Deleted Item' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-// Middleware to get item by ID
-async function getItem(req, res, next) {
-  let item;
-  try {
-    item = await Item.findById(req.params.id);
-    if (item == null) {
-      return res.status(404).json({ message: 'Cannot find item' });
+    const item = new Item({
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      quantity: req.body.quantity,
+      category: req.body.category,
+      manufacturer: req.body.manufacturer,
+      releaseDate: req.body.releaseDate,
+      isAvailable: req.body.isAvailable
+    });
+  
+    try {
+      const newItem = await item.save();
+      res.status(201).json(newItem);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
     }
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
-
-  res.item = item;
-  next();
-}
-
-module.exports = router;
+  });
+  
