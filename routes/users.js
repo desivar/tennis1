c32a6bc01812e8656244c4c79373efd2de9541a8
desivar <jillian_desire@yahoo.es>
2025-04-12 03:15:47 -0600

    rotes/users.js

---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// ... (GET, POST, PUT, DELETE routes for users)
+
+module.exports = router;
