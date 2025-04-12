d5a1bad0f717024f26a6bda0c94aece425b2dd70
desivar <jillian_desire@yahoo.es>
2025-04-12 03:39:06 -0600

    fix the server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const usersRoutes = require('./routes/users');
 const app = express();
 const port = process.env.PORT || 5500;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
