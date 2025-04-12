01677ff665ea31e57d6dd139b8cac7b54cdd2111
desivar <jillian_desire@yahoo.es>
2025-04-12 03:14:44 -0600

    routes/items

---
+++ routes/items.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('../models/Item');
+
+// ... (GET, POST, PUT, DELETE routes for items)
+
+module.exports = router;
