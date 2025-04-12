ece20fc7c93ab151cc1cb0aa11e78b3f30e7fc27
desivar <jillian_desire@yahoo.es>
2025-04-12 03:47:32 -0600

    items/users

--- routes/items.js
+++ routes/items.js
@@ -2,6 +2,77 @@ const express = require('express');
 const router = express.Router();
 const Item = require('../models/Item');
 
-// ... (GET, POST, PUT, DELETE routes for items)
+// GET all items
+router.get('/', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.json(items);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET one item
+router.get('/:id', getItem, (req, res) => {
+  res.json(res.item);
+});
+
+// POST a new item
+router.post('/', async (req, res) => {
+  const item = new Item({
+    name: req.body.name,
+    description: req.body.description
+  });
+
+  try {
+    const newItem = await item.save();
+    res.status(201).json(newItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// PUT (update) an item
+router.put('/:id', getItem, async (req, res) => {
+  if (req.body.name != null) {
+    res.item.name = req.body.name;
+  }
+  if (req.body.description != null) {
+    res.item.description = req.body.description;
+  }
+
+  try {
+    const updatedItem = await res.item.save();
+    res.json(updatedItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE an item
+router.delete('/:id', getItem, async (req, res) => {
+  try {
+    await res.item.remove();
+    res.json({ message: 'Deleted Item' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Middleware to get item by ID
+async function getItem(req, res, next) {
+  let item;
+  try {
+    item = await Item.findById(req.params.id);
+    if (item == null) {
+      return res.status(404).json({ message: 'Cannot find item' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.item = item;
+  next();
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,78 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-// ... (GET, POST, PUT, DELETE routes for users)
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET one user by ID
+router.get('/:id', getUser, (req, res) => {
+  res.json(res.user);
+});
+
+// POST a new user
+router.post('/', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    // Add other fields as needed
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// PUT (update) a user by ID
+router.put('/:id', getUser, async (req, res) => {
+  if (req.body.name != null) {
+    res.user.name = req.body.name;
+  }
+  if (req.body.email != null) {
+    res.user.email = req.body.email;
+  }
+
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE a user by ID
+router.delete('/:id', getUser, async (req, res) => {
+  try {
+    await res.user.remove();
+    res.json({ message: 'Deleted User' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Middleware to get a user by ID
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: 'Cannot find user' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.user = user;
+  next();
+}
 
 module.exports = router;
